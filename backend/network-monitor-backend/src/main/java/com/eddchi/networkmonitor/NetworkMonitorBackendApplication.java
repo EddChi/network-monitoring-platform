257ce626566abdfb07ac57059bedd432e9b20064
EddChi <eddychi98@icloud.com>
2026-05-24 10:32:42 +0100

    feat: create Spring Boot backend
    Create Spring Boot backend with PostgreSQL connection, Gradle setup and Docker Compose database configuration.

---
+++ backend/network-monitor-backend/src/main/java/com/eddchi/networkmonitor/NetworkMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eddchi.networkmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkMonitorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetworkMonitorBackendApplication.class, args);
+    }
+
+}
---
+++ backend/network-monitor-backend/src/test/java/com/eddchi/networkmonitor/NetworkMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddchi.networkmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkMonitorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
